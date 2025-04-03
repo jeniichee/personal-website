68dd7210ef590c85bc2976268afb90df8b85287e
Jennifer Cheung <jennifercheung1234@gmail.com>
2025-04-03 19:12:25 -0400

    fixed project background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
@@ -8,21 +7,20 @@ import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Yuki from "./components/Yuki/Yuki";
 import Contact from "./components/Contact/Contact";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/yuki" element={<Yuki />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Yuki />
+      <Contact />
+    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,11 +2,20 @@ import React from "react";
 import "./Portfolio.css";
 
 function Portfolio() {
+
   return (
-    <section id="portfolio">
+    <section className="portfolio">
       <h2 className="major">Portfolio</h2>
-      <div className="project">
-        {/* Add each project as a component or an object */}
+      <div className="projects">
+        <div className="card" > 
+          <div className="card-front">
+            This is the front of the card.
+            <div className="card-back">
+              This is the back of the card. 
+            </div>
+          </div>
+        </div>
+        
       </div>
     </section>
   );
@@ -18,3 +27,4 @@ export default Portfolio;
 // click on each project to learn more about the project/takeaways/blog+code snippets
 // // takes u to a different page
 // have a small section to request full code
+// project cards 
