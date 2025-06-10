3352e55e201bd924de05ec403e51b6ee7808991d
Jennifer Cheung <jennifercheung1234@gmail.com>
2025-06-10 19:36:35 -0400

    project card fronts

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -12,56 +12,55 @@ const Portfolio = () => {
             <span className="front-title">My Portfolio</span>
             <span>Personal Project</span>
           </span>
-          <span className="back">Still in works!</span>
+          <span className="back">
+            This website! Coded using React, HTML, CSS, and JavaScript. I've
+            integrated a few design choices I've learned from Fundamentals of
+            Software Engineering, and I'm hoping to add more features in the
+            near future. Still have soo many more ideas to try for this project!
+          </span>
         </div>
 
         {/* Fake Stack OverFlow */}
         <div className="card">
           <span className="front">
-            <span className="front-title">My Portfolio</span>
-            <span>Personal Project</span>
+            <span className="front-title">AnswerHub</span>
+            <span>Academic Case Study</span>
           </span>
-          <span className="back">Still in works!</span>
+          <span className="back">
+    
+          </span>
         </div>
 
         {/* NLP Text Classification */}
         <div className="card">
           <span className="front">
-            <span className="front-title">My Portfolio</span>
-            <span>Personal Project</span>
+            <span className="front-title">NLP Text Classification</span>
+            <span>Academic Case Study</span>
           </span>
-          <span className="back">Still in works!</span>
+          <span className="back"></span>
         </div>
 
         {/* Drug Consumption Project */}
         <div className="card">
           <span className="front">
-            <span className="front-title">My Portfolio</span>
-            <span>Personal Project</span>
+            <span className="front-title">Drug Consumption</span>
+            <span>Academic Case Study</span>
           </span>
-          <span className="back">Still in works!</span>
+          <span className="back"></span>
         </div>
 
-        {/*  */}
+        {/* 2048 AI */}
         <div className="card">
           <span className="front">
-            <span className="front-title">My Portfolio</span>
-            <span>Personal Project</span>
+            <span className="front-title">2048 AI</span>
+            <span>Academic Case Study</span>
           </span>
-          <span className="back">Still in works!</span>
-        </div>
-
-        <div className="card">
-          <span className="front">
-            <span className="front-title">My Portfolio</span>
-            <span>Personal Project</span>
-          </span>
-          <span className="back">Still in works!</span>
+          <span className="back"></span>
         </div>
 
         <div className="card">
           <span className="front">
-            <span className="front-title">My Portfolio</span>
+            <span className="front-title">Attendance Manager System</span>
             <span>Personal Project</span>
           </span>
           <span className="back">Still in works!</span>
@@ -69,7 +68,7 @@ const Portfolio = () => {
 
         <div className="card">
           <span className="front">
-            <span className="front-title">My Portfolio</span>
+            <span className="front-title">Maze</span>
             <span>Personal Project</span>
           </span>
           <span className="back">Still in works!</span>
@@ -77,7 +76,15 @@ const Portfolio = () => {
 
         <div className="card">
           <span className="front">
-            <span className="front-title">My Portfolio</span>
+            <span className="front-title">Bullet Journal</span>
+            <span>Personal Project</span>
+          </span>
+          <span className="back">Still in works!</span>
+        </div>
+
+        <div className="card">
+          <span className="front">
+            <span className="front-title">BattleSalvo</span>
             <span>Personal Project</span>
           </span>
           <span className="back">Still in works!</span>
