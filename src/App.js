443ae51dcd273a253cf14d16e9ecc2f8bcd8bb35
Jennifer Cheung <jennifercheung1234@gmail.com>
2025-06-10 23:51:52 -0400

    fixed footnote icons

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
             }
           />
 
+
           <Route
             path="/about"
             element={
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaEnvelope, FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa6";
 import "./Contact.css";
 
 const Contact = () => {
@@ -6,24 +7,25 @@ const Contact = () => {
     <section id="contact">
       <div className="copyright">
         <div className="copy-left">
-          <h2 className="major">Thank you for visiting!</h2>
+          <h2 className="major">Thank you for visiting</h2>
           <div className="copy-comfy">I hope your stay was nice and comfy!</div>
         </div>
 
         <div className="copy-right">
           <h2 className="major">What's Next?</h2>
+          <span>Let's connect!</span>
           <div className="socials">
             <a href="mailto:jennifercheung1234@gmail.com">
-              <img src="assets/images/email.png" alt="Email" />
+              <FaEnvelope />
             </a>
             <a href="https://www.linkedin.com/in/jennifer-cheung1/">
-              <img src="assets/images/linkedin.png" alt="LinkedIn" />
+              <FaLinkedin />
             </a>
             <a href="https://github.com/jeniichee">
-              <img src="assets/images/github.png" alt="GitHub" />
+              <FaGithub />
             </a>
             <a href="https://www.instagram.com/jeniichee/">
-              <img src="assets/images/instagram.png" alt="Instagram" />
+              <FaInstagram />
             </a>
           </div>
         </div>
