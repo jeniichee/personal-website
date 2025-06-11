6ac54d49d61819fd8e6513cb3dd064a5b73d7d50
Jennifer Cheung <jennifercheung1234@gmail.com>
2025-06-10 22:30:53 -0400

    created popups for more information regarding project

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,95 +1,58 @@
-import React from "react";
+import { React, useState } from "react";
+import Popup from "../Popup/Popup";
+
 import "./Portfolio.css";
 
 const Portfolio = () => {
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState("");
+
+  const projects = [
+    {
+      id: 0,
+      title: "My Portfolio",
+      type: "Personal Project",
+      summary:
+        "This website! Coded using React, HTML, CSS, and JavaScript. I've integrated a few web dev choices I've learned from Fundamentals of Software Engineering, and I'm hoping to add more features in the near future. Still have soo many more ideas to try for this project!",
+      description: "hello this is portfolio. ",
+    },
+    {
+      id: 1,
+      title: "AnswerHub",
+      description: "hello this is answerhub",
+    },
+  ];
+
+  const handlePopup = (projectId) => {
+    setShowPopup(true);
+    const project = projects.find(p => p.id === projectId)
+    setPopupMessage(project.description);
+  };
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
+
   return (
     <section id="portfolio">
       <h2 className="major">Projects</h2>
       <div className="projects" style={{ paddingTop: "30px" }}>
         {/* Personal Website */}
-        <div className="card">
-          <span className="front">
-            <span className="front-title">My Portfolio</span>
-            <span>Personal Project</span>
-          </span>
-          <span className="back">
-            This website! Coded using React, HTML, CSS, and JavaScript. I've
-            integrated a few design choices I've learned from Fundamentals of
-            Software Engineering, and I'm hoping to add more features in the
-            near future. Still have soo many more ideas to try for this project!
-          </span>
-        </div>
-
-        {/* Fake Stack OverFlow */}
-        <div className="card">
-          <span className="front">
-            <span className="front-title">AnswerHub</span>
-            <span>Academic Case Study</span>
-          </span>
-          <span className="back">
-    
-          </span>
-        </div>
-
-        {/* NLP Text Classification */}
-        <div className="card">
-          <span className="front">
-            <span className="front-title">NLP Text Classification</span>
-            <span>Academic Case Study</span>
-          </span>
-          <span className="back"></span>
-        </div>
-
-        {/* Drug Consumption Project */}
-        <div className="card">
-          <span className="front">
-            <span className="front-title">Drug Consumption</span>
-            <span>Academic Case Study</span>
-          </span>
-          <span className="back"></span>
-        </div>
-
-        {/* 2048 AI */}
-        <div className="card">
-          <span className="front">
-            <span className="front-title">2048 AI</span>
-            <span>Academic Case Study</span>
-          </span>
-          <span className="back"></span>
-        </div>
-
-        <div className="card">
-          <span className="front">
-            <span className="front-title">Attendance Manager System</span>
-            <span>Personal Project</span>
-          </span>
-          <span className="back">Still in works!</span>
-        </div>
-
-        <div className="card">
-          <span className="front">
-            <span className="front-title">Maze</span>
-            <span>Personal Project</span>
-          </span>
-          <span className="back">Still in works!</span>
-        </div>
-
-        <div className="card">
-          <span className="front">
-            <span className="front-title">Bullet Journal</span>
-            <span>Personal Project</span>
-          </span>
-          <span className="back">Still in works!</span>
-        </div>
-
-        <div className="card">
-          <span className="front">
-            <span className="front-title">BattleSalvo</span>
-            <span>Personal Project</span>
-          </span>
-          <span className="back">Still in works!</span>
-        </div>
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="card"
+            onClick={() => handlePopup(project.id)}
+          >
+            <span className="front">
+              <span className="project-title">{project.title}</span>
+              <span className="project-type">{project.type}</span>
+            </span>
+            <span className="back">{project.summary}</span>
+          </div>
+        ))}
       </div>
+      {showPopup && <Popup message={popupMessage} onClose={handlePopupClose} />}
     </section>
   );
 };
