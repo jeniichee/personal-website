e17627f2f125e2cc54c38cefdf8bf51a47746d3d
Jennifer Cheung <jennifercheung1234@gmail.com>
2025-06-13 22:25:58 -0400

    updated me

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -74,7 +74,7 @@ const Home = () => {
             <ReactMarkedDown>
               Wearing the winged sandals of Hermes, I embody the "jack of all
               trades" spirit with a dash of playful energy: **fullstack/frontend
-              engineer**, **data engineer**, and a love for **product design**!
+              engineer**, **data engineer**, and a keen interest for **cybersecurity**!
             </ReactMarkedDown>
           </span>
         </div>
